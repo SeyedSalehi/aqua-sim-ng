4bddc90f09ee5288bbefd595b9303a1513a018bd
Seyedmohammad Salehi <salehi@udel.edu>
2016-09-20 22:38:18 -0700

    Update aqua-sim-routing-dbr.h
    Change QueueItem class definition to QueueItem2 due to a confict in NS3.25 which leads to class redefinition error.

--- model/aqua-sim-routing-dbr.h
+++ model/aqua-sim-routing-dbr.h
@@ -83,14 +83,14 @@ private:
   AquaSimDBR *m_a;
 };  // class DBR_SendingTimer
 
-class QueueItem : public Object {
+class QueueItem2 : public Object {
 public:
-	QueueItem() : /*m_p(0),*/ m_sendTime(0) {}
-	QueueItem(Ptr<Packet> p, double t) : m_p(p), m_sendTime(t) {}
+	QueueItem2() : /*m_p(0),*/ m_sendTime(0) {}
+	QueueItem2(Ptr<Packet> p, double t) : m_p(p), m_sendTime(t) {}
 
 	Ptr<Packet> m_p;		// pointer to the packet
 	double m_sendTime;	// time to send the packet
-};  // class QueueItem
+};  // class QueueItem2
 
 class MyPacketQueue : public Object {
 public:
@@ -102,13 +102,13 @@ public:
 	void dump();
 
 	void pop() { m_dq.pop_front(); };
-	QueueItem* front() { return m_dq.front(); };
-	void insert(QueueItem* q);
+	QueueItem2* front() { return m_dq.front(); };
+	void insert(QueueItem2* q);
 	bool update(Ptr<Packet> p, double t);
 	bool purge(Ptr<Packet> p);
 
 private:
-	std::deque<QueueItem*> m_dq;
+	std::deque<QueueItem2*> m_dq;
 };  // class MyPacketQueue
 
 class NeighbEnt{
